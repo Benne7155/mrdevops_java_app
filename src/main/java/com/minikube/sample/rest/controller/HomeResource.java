9fc90950480b4991c2944a7ddd1892520b76f681
Benne7155 <142221199+Benne7155@users.noreply.github.com>
2023-08-15 17:20:29 +0000

    file renamed

--- src/main/java/com/minikube/sample/rest/controller/HomeResource.java
+++ src/main/java/com/minikube/sample/rest/controller/HomeResource.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @created 06-12-2018
  */
 @RestController
-@RequestMapping("/home1")
+@RequestMapping("/home")
 public class HomeResource {
 
 	@Autowired
